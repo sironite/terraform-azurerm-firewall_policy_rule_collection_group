8c3da8ebdd2136be13738aea21d695748d3dfbf2
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-26 09:42:17 +0200

    fix: module

--- main.tf
+++ main.tf
@@ -3,40 +3,43 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
   firewall_policy_id = var.firewall_policy_id
   priority           = var.priority
 
-
   dynamic "application_rule_collection" {
-    for_each = try(var.firewall_policy_rules.application_rule_collections, {})
+    for_each = var.application_rule_collections != null ? var.application_rule_collections : {}
 
     content {
-      name     = application_rule_collection.value.name
+      name = application_rule_collection.value.name
       priority = application_rule_collection.value.priority
-      action   = application_rule_collection.value.action
+      action = application_rule_collection.value.action
 
-      dynamic "rule" {
-        for_each = try(application_rule_collection.value.rules, {})
+    dynamic "rule" {
+      for_each = application_rule_collection.value.rules != null ? application_rule_collection.value.rules : {}
 
-        content {
-          name                  = rule.value.name
-          source_addresses      = try(rule.value.source_addresses, null)
-          source_ip_groups      = try(rule.value.source_ip_groups, null)
-          destination_fqdn_tags = try(rule.value.destination_fqdn_tags, null)
-          destination_fqdns     = try(rule.value.destination_fqdns, null)
+      content {
+        name = rule.value.name
+        description = try(rule.value.description, null)
+        source_addresses = try(rule.value.source_addresses, null)
+        source_ip_groups = try(rule.value.source_ip_groups, null)
+        destination_addresses = try(rule.value.destination_addresses, null)
+        destination_urls = try(rule.value.destination_urls, null)
+        destination_fqdns = try(rule.value.destination_fqdns, null)
+        terminate_tls = try(rule.value.terminate_tls, null)
+        web_categories = try(rule.value.web_categories, null)
 
-          dynamic "protocols" {
-            for_each = try(rule.value.protocols, {})
+        dynamic "protocols" {
+          for_each = rule.value.protocols != null ? rule.value.protocols : {}
 
-            content {
-              type = protocols.value.type
-              port = try(protocols.value.port, null)
-            }
+          content {
+            port = protocols.value.port
+            type = protocols.value.type
           }
         }
-      }
+      }  
+    }
     }
   }
 
   dynamic "network_rule_collection" {
-    for_each = try(each.value.network_rule_collections, {})
+    for_each = var.network_rule_collections != null ? var.network_rule_collections : {}
 
     content {
       name     = network_rule_collection.value.name
@@ -44,11 +47,10 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
       action   = network_rule_collection.value.action
 
       dynamic "rule" {
-        for_each = try(network_rule_collection.value.rules, {})
+        for_each = network_rule_collection.value.rules != null ? network_rule_collection.value.rules : {}
 
         content {
           name                  = rule.value.name
-          description           = try(rule.value.description, null)
           source_addresses      = try(rule.value.source_addresses, null)
           source_ip_groups      = try(rule.value.source_ip_groups, null)
           destination_addresses = try(rule.value.destination_addresses, null)
@@ -61,29 +63,25 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
     }
   }
 
-  dynamic "nat_rule_collection" {
-    for_each = try(var.firewall_policy_rules.nat_rule_collections, {})
+  # dynamic "nat_rule_collection" {
+  #   for_each = var.nat_rule_collections != null ? var.nat_rules : {}
 
-    content {
-      name     = azurecaf_name.nat_rule[nat_rule_collection.key].result
-      priority = nat_rule_collection.value.priority
-      action   = nat_rule_collection.value.action
+  #   content {
+  #     name = nat_rule_collections.value.name
+  #     action = nat_rule_collections.value.action
+  #     priority = nat_rule_collections.value.priority
 
-      dynamic "rule" {
-        for_each = try(nat_rule_collection.value.rules, {})
-
-        content {
-          name                = rule.value.name
-          source_addresses    = try(rule.value.source_addresses, null)
-          source_ip_groups    = try(rule.value.source_ip_groups, null)
-          destination_ports   = try(rule.value.destination_ports, null)
-          destination_address = try(rule.value.destination_address, null)
-          translated_port     = rule.value.translated_port
-          translated_address  = rule.value.translated_address
-          protocols           = rule.value.protocols
-        }
-      }
-    }
-  }
-
+  #     dynamic "rule"{
+  #       name = nat_rule_collections.value.rule.name
+  #       protocol = nat_rule_collections.value.rule.protocol
+  #       source_addresses = try(nat_rule_collections.value.rule.source_addresses, null)
+  #       source_ip_groups = try(nat_rule_collections.value.rule.source_ip_groups, null)
+  #       destination_addresses = try(nat_rule_collections.value.rule.destination_addresses, null)
+  #       destination_ports =try(nat_rule_collections.value.rule.destination_ports, null)
+  #       translated_address = nat_rule_collections.value.rule.translated_address
+  #       translated_fqdn = try(nat_rule_collections.value.rule.translated_fqdn, null)
+  #       translated_port = try(nat_rule_collections.value.rule.translated_port, null)
+  #     }
+  #   }    
+  # }
 }
